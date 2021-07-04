7e112cdf6199311836d11f835869073cf4492e9e
Oleksand Andreiev <andreyew89gmail.com>
2021-07-04 20:05:53 +0300

    error during fetching Ingredient data has been fixed.

--- src/main/java/tacos/Ingredient.java
+++ src/main/java/tacos/Ingredient.java
@@ -4,8 +4,8 @@ import lombok.AccessLevel;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+
+import javax.persistence.*;
 
 @Data
 @RequiredArgsConstructor
@@ -13,11 +13,15 @@ import javax.persistence.Id;
 @Entity
 public class Ingredient {
     @Id
+    @Column(columnDefinition = "varchar(4)")
     private final String id;
+    @Column(columnDefinition = "varchar(25)")
     private final String name;
+    @Column(columnDefinition = "varchar(10)")
+    @Enumerated(EnumType.STRING)
     private final Type type;
 
-    public static enum Type {
+    public enum Type {
         WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
     }
 }
