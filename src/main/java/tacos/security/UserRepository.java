47135fb23bc4baf21ae50420d0aa7c31e249a8e5
oleksandr.andreiev <andreyew89@gmail.com>
2024-10-07 21:22:45 +0300

    minor corrections

--- src/main/java/tacos/security/UserRepository.java
+++ src/main/java/tacos/security/UserRepository.java
@@ -2,7 +2,9 @@ package tacos.security;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<User, Long> {
 
-    User findByUsername(String username);
+    Optional<User> findByUsername(String username);
 }
