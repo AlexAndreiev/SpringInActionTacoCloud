8916687091a7b54e0ac794d26955e1abfc13e5b8
oleksandr.andreiev <andreyew89@gmail.com>
2024-10-08 18:03:44 +0300

    minor corrections

--- src/main/java/tacos/TacoCloudApplication.java
+++ src/main/java/tacos/TacoCloudApplication.java
@@ -1,7 +1,13 @@
 package tacos;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import tacos.data.IngredientRepository;
+import tacos.security.UserRepository;
 
 @SpringBootApplication
 public class TacoCloudApplication {
@@ -10,4 +16,11 @@ public class TacoCloudApplication {
 		SpringApplication.run(TacoCloudApplication.class, args);
 	}
 
+//	@Bean
+//	@Profile({"dev", "qa"})
+//	public CommandLineRunner dataLoader(IngredientRepository repo, UserRepository userRepo,
+//										PasswordEncoder encoder)
+//	{
+//
+//	}
 }
