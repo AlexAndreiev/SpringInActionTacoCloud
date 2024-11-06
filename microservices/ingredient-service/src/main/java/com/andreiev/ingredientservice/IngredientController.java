2bc54b0b089d24edba872d27ca05f3b38e59468d
oleksandr.andreiev <andreyew89@gmail.com>
2024-11-06 16:47:00 +0200

    tmp

---
+++ microservices/ingredient-service/src/main/java/com/andreiev/ingredientservice/IngredientController.java
@@ -0,0 +1,16 @@
+package com.andreiev.ingredientservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping( "/ingredients")
+public class IngredientController {
+
+    @GetMapping("/{id}")
+    public Ingredient getIngredient(@PathVariable int id) {
+        return new Ingredient("Some code", "Test ingredient");
+    }
+}
